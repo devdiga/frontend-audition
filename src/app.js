e7e5e9b5f8622039558b6c0ef5ced25a30ff8420
Bruno Lucho Macedo <bmacedo@weg.net>
2021-12-11 02:33:23 -0300

    Implementada função para carregar mais fotos.

--- src/app.js
+++ src/app.js
@@ -3,13 +3,35 @@ import '/src/assets/page.css';
 
 import photoCard from './components/photoCard';
 
-axios.get('https://jsonplaceholder.typicode.com/photos')
-  .then((response) => {
-    const container = document.getElementById('container');
-    response.data.slice(0, 20).forEach((photo) => {
-      container.appendChild(photoCard(photo));
+var elementNum = 20;
+
+if (elementNum = 20) {
+  axios.get('https://jsonplaceholder.typicode.com/photos')
+    .then((response) => {
+      const container = document.getElementById('container');
+      response.data.slice(0, elementNum).forEach((photo) => {
+        container.appendChild(photoCard(photo));
+      });
+    })
+    .catch((error) => {
+      alert(error);
     });
-  })
-  .catch((error) => {
-    alert(error);
-  });
+};
+
+document.getElementById('load-more').addEventListener('click', loadMore);
+
+function loadMore() {
+  elementNum += 20;
+  if(elementNum <= 500) {
+    axios.get('https://jsonplaceholder.typicode.com/photos')
+      .then((response) => {
+        const container = document.getElementById('container');
+        response.data.slice(0, elementNum).forEach((photo) => {
+          container.appendChild(photoCard(photo));
+        });
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  };
+};
