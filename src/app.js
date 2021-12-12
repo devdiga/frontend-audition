929c77e264e2c9a840382dc62001c2b15fc2133e
Bruno Lucho Macedo <bmacedo@weg.net>
2021-12-11 23:48:39 -0300

    Alterada a forma de carregamento da página de acordo com compo de busca

--- src/app.js
+++ src/app.js
@@ -4,34 +4,53 @@ import '/src/assets/page.css';
 import photoCard from './components/photoCard';
 
 var elementNum = 20;
+var searching = 0;
 
-if (elementNum = 20) {
-  axios.get('https://jsonplaceholder.typicode.com/photos')
-    .then((response) => {
-      const container = document.getElementById('container');
-      response.data.slice(0, elementNum).forEach((photo) => {
-        container.appendChild(photoCard(photo));
-      });
-    })
-    .catch((error) => {
-      alert(error);
-    });
-};
+//Valida se o campo de busca está sendo preenchido
+/*
+searching = document.getElementById('search').addEventListener("input", ({target}) => {
+    const searchInput = target.value
+    if (searchInput !== '') {
+        return 1;
+    }
+
+    else {
+        return 0;
+    }
+});
+*/
+//Caso o usuário não pressione o botão "Carregar Mais" ou preencha o campo de busca, a página exibe as primeiras 20 fotos:
+
+if (elementNum == 20 && document.getElementById('search').onload !== 0)
+    axios.get('https://jsonplaceholder.typicode.com/photos')
+        .then((response) => {
+            const container = document.getElementById('container');
+            response.data.slice(0, elementNum).forEach((photo) => {
+                container.appendChild(photoCard(photo));
+            });
+        })
+        .catch((error) => {
+            alert(error);
+        });
+        
+//Se o botão "Carregar Mais" for pressionado e não estiver sendo feita nenhuma busca, a página exibe mais 20 fotos:
 
 document.getElementById('load-more').addEventListener('click', loadMore);
 
 function loadMore() {
-  elementNum += 20;
-  if(elementNum <= 500) {
-    axios.get('https://jsonplaceholder.typicode.com/photos')
-      .then((response) => {
-        const container = document.getElementById('container');
-        response.data.slice(0, elementNum).forEach((photo) => {
-          container.appendChild(photoCard(photo));
-        });
-      })
-      .catch((error) => {
-        alert(error);
-      });
-  };
+    elementNum += 20;
+    if (elementNum <= 500 && document.getElementById('search').oninput !== 0) {
+        axios.get('https://jsonplaceholder.typicode.com/photos')
+            .then((response) => {
+                const container = document.getElementById('container');
+                response.data.slice(0, elementNum).forEach((photo) => {
+                    container.appendChild(photoCard(photo));
+                });
+            })
+            .catch((error) => {
+                alert(error);
+            });
+    };
 };
+
+//document.getElementById('search').includes
--- src/components/photoCard.js
+++ src/components/photoCard.js
@@ -1,14 +1,14 @@
 import '../assets/photo-card.css';
 
 export default function photoCard(item) {
-  const img = document.createElement('img');
-  img.src = item.url;
-  const txt = document.createElement('div');
-  txt.innerHTML = item.title;
-  txt.className = 'title';
-  const card = document.createElement('div');
-  card.className = 'photo-card';
-  card.appendChild(img);
-  card.appendChild(txt);
-  return card;
+    const img = document.createElement('img');
+    img.src = item.url;
+    const txt = document.createElement('div');
+    txt.innerHTML = item.title;
+    txt.className = 'title';
+    const card = document.createElement('div');
+    card.className = 'photo-card';
+    card.appendChild(img);
+    card.appendChild(txt);
+    return card;
 }
