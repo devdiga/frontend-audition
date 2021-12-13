c8a3c5db5dd2b9e771214be27316b67a51eeb7d9
Bruno Lucho Macedo <bmacedo@weg.net>
2021-12-13 18:33:57 -0300

    Impletada a função de busca / autocomplete.

--- src/app.js
+++ src/app.js
@@ -3,47 +3,41 @@ import '/src/assets/page.css';
 
 import photoCard from './components/photoCard';
 
-var elementNum = 20;
-var searching = 0;
-
-//Valida se o campo de busca está sendo preenchido
-/*
-searching = document.getElementById('search').addEventListener("input", ({target}) => {
-    const searchInput = target.value
-    if (searchInput !== '') {
-        return 1;
-    }
-
-    else {
-        return 0;
-    }
-});
-*/
 //Caso o usuário não pressione o botão "Carregar Mais" ou preencha o campo de busca, a página exibe as primeiras 20 fotos:
 
-if (elementNum == 20 && document.getElementById('search').onload !== 0)
-    axios.get('https://jsonplaceholder.typicode.com/photos')
-        .then((response) => {
-            const container = document.getElementById('container');
-            response.data.slice(0, elementNum).forEach((photo) => {
-                container.appendChild(photoCard(photo));
+var searching = false;
+var firstEl = 0;
+var lastEl = 20;
+
+function initialPage() {
+    document.getElementById('container').innerHTML = '';
+    firstEl = 0;
+    lastEl = 20;
+    if (lastEl === 20 && searching === false) {
+        axios.get('https://jsonplaceholder.typicode.com/photos')
+            .then((response) => {
+                const container = document.getElementById('container');
+                response.data.slice(firstEl, lastEl).forEach((photo) => {
+                    container.appendChild(photoCard(photo));
+                });
+            })
+            .catch((error) => {
+                alert(error);
             });
-        })
-        .catch((error) => {
-            alert(error);
-        });
-        
+    };
+};    
 //Se o botão "Carregar Mais" for pressionado e não estiver sendo feita nenhuma busca, a página exibe mais 20 fotos:
 
 document.getElementById('load-more').addEventListener('click', loadMore);
 
 function loadMore() {
-    elementNum += 20;
-    if (elementNum <= 500 && document.getElementById('search').oninput !== 0) {
+    firstEl += 20;
+    lastEl += 20; // Implementar o firstEl and lastEl
+    if (lastEl <= 500 && searching === false) {
         axios.get('https://jsonplaceholder.typicode.com/photos')
             .then((response) => {
                 const container = document.getElementById('container');
-                response.data.slice(0, elementNum).forEach((photo) => {
+                response.data.slice(firstEl, lastEl).forEach((photo) => {
                     container.appendChild(photoCard(photo));
                 });
             })
@@ -53,4 +47,30 @@ function loadMore() {
     };
 };
 
-//document.getElementById('search').includes
+//Busca automática (autocomplete)
+document.getElementById('search').addEventListener('input', ({target})  => {
+    const searchInput = target.value;
+    searching = true;
+    axios.get('https://jsonplaceholder.typicode.com/photos')  
+        .then((response) => {
+            let filtered = response.data.filter(({title}) => title.indexOf(searchInput) >= 0);
+            const container = document.getElementById('container');
+            filtered.forEach((photo) => {
+                container.appendChild(photoCard(photo));
+            });
+        })
+        .catch((error) => {
+            alert(error);
+        });
+});
+
+function deleteSearch () {
+    document.getElementById('container').innerHTML = '';
+}
+
+document.getElementById('search').addEventListener('input', deleteSearch);
+
+
+if (searching === false)
+    initialPage();
+    console.log(searching);
