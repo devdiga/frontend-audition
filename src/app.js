133595c825de5246871a34fd3343974a3224487d
Bruno Lucho Macedo <bmacedo@weg.net>
2021-12-15 13:31:22 -0300

    Retiradas as variáveis que representavam de forma errado os estados dos eventos.

--- src/app.js
+++ src/app.js
@@ -4,47 +4,57 @@ import '/src/assets/page.css';
 import photoCard from './components/photoCard';
 import photoList from './components/photoList';
 
-//Caso o usuário não pressione o botão "MOSTRAR MAIS" ou preencha o campo de busca, a página exibe as primeiras 20 fotos:
-
-var searching = false;
-var listViewOn = false;
 var firstEl = 0;
 var lastEl = 20;
 
-if (searching === false && listViewOn === false)
-    initialPage();
+// Todas as Fotos
+var photos = []
+
+//Total de fotos
+var totalPhotos = null;
+
+//Fotos filtradas de acordo com o input
+var filtered = []
+
+//Container onde são carregadas as fotos
+const container = document.getElementById('container');
+
+renderPhotos();
 
 function getPhotos () {
     axios.get('https://jsonplaceholder.typicode.com/photos')
             .then((response) => {
-                const container = document.getElementById('container');
-                response.data.slice(firstEl, lastEl).forEach((photo) => {
+                photos = response.data;
+                photos.slice(firstEl, lastEl).forEach((photo) => {
                     container.appendChild(photoCard(photo));
+                totalPhotos = photos.length;
                 var p = document.getElementById('element-num');
-                p.innerText =  `Mostrando ${lastEl} de 500`;;
+                p.innerText =  `Mostrando ${lastEl} de ${totalPhotos}`;
                 });
             })
             .catch((error) => {
                 alert(error);
             });
+    return photos;
 }
 
-function initialPage() {
-    listViewOn = false;
-    document.getElementById('container').innerHTML = '';
+function renderPhotos() {
     firstEl = 0;
     lastEl = 20;
-    getPhotos();
+    getPhotos();  
 };
 
 //Botão "MOSTRAR MAIS" pressionado
-document.getElementById('load-more').addEventListener('click', loadMore);
+//document.getElementById('load-more').addEventListener('click', loadMore);
 
+/*
 function loadMore() {
     firstEl += 20; //firstEl and lastEl
     lastEl += 20;
-
+}
+*/
     //Se o botão "MOSTRAR MAIS" for pressionado na exibição de bloco e não estiver sendo feita nenhuma busca, a página exibe mais 20 fotos:
+    /*
     if (lastEl <= 500 && searching === false && listViewOn === false) {
         axios.get('https://jsonplaceholder.typicode.com/photos')
             .then((response) => {
@@ -52,15 +62,17 @@ function loadMore() {
                 response.data.slice(firstEl, lastEl).forEach((photo) => {
                     container.appendChild(photoCard(photo));
                     var p = document.getElementById('element-num');
-                    p.innerText =  `Mostrando ${lastEl} de 500`;;
+                    p.innerText =  `Mostrando ${lastEl} de 500`;
                 });
             })
             .catch((error) => {
                 alert(error);
             });
     };
+    */
 
     //Se o botão "MOSTRAR MAIS" for pressionado na exibição de linha e não estiver sendo feita nenhuma busca, a página exibe mais 20 linhas:
+    /*
     if (lastEl <= 500 && searching === false && listViewOn === true) {
         axios.get('https://jsonplaceholder.typicode.com/photos')
             .then((response) => {
@@ -76,17 +88,29 @@ function loadMore() {
             });
     };
 };
-
+*/
 //Busca automática (autocomplete)
 
 document.getElementById('search').addEventListener('input', ({target})  => {
     const searchInput = target.value;
-    searching = true;
+    filtered = photos.filter(({title}) => title.indexOf(searchInput) >= 0);
+    console.log(filtered);
+    container.innerHTML = '';
+    filtered.slice(0, lastEl).forEach((photo) => {
+        container.appendChild(photoCard(photo));
+        totalPhotos = filtered.length;
+        var p = document.getElementById('element-num');
+        p.innerText =  `Mostrando ${lastEl} de ${totalPhotos}`;
+    });
 
+});
+
+    
+    /*
     if (listViewOn === false) {
         axios.get('https://jsonplaceholder.typicode.com/photos')  
             .then((response) => {
-                let filtered = response.data.filter(({title}) => title.indexOf(searchInput) >= 0);
+                let filtered = .filter(({title}) => title.indexOf(searchInput) >= 0);
                 const container = document.getElementById('container');
                 filtered.forEach((photo) => {
                     container.appendChild(photoCard(photo));
@@ -98,7 +122,9 @@ document.getElementById('search').addEventListener('input', ({target})  => {
                 alert(error);
             });
     }
+    */
 
+    /*
     if (listViewOn === true) {
         axios.get('https://jsonplaceholder.typicode.com/photos')
             .then((response) => {
@@ -107,7 +133,7 @@ document.getElementById('search').addEventListener('input', ({target})  => {
                 filtered.forEach((photo) => {
                     container.appendChild(photoList(photo));
                     var p = document.getElementById('element-num');
-                    p.innerText =  `Mostrando ${lastEl} de 500`;;
+                    p.innerText =  `Mostrando ${lastEl} de 500`;
                 });
             })
             .catch((error) => {
@@ -115,25 +141,17 @@ document.getElementById('search').addEventListener('input', ({target})  => {
             });
     }
 });
-
-//Toda vez que digitar / deletar um caractere no input limpa a tela, para não ficar buscas anteriores:
-
-document.getElementById('search').addEventListener('input', deleteSearch);
-
-function deleteSearch () {
-    document.getElementById('container').innerHTML = '';
-}
+*/
 
 //Visualização por bloco
 
-document.getElementById('block').addEventListener("click", initialPage);
+//document.getElementById('block').addEventListener("click", initialPage);
 
 //Visualização por lista
 
 document.getElementById('list').addEventListener("click", listView);
 
 function listView () {
-    listViewOn = true;
     firstEl = 0;
     lastEl = 20;
     document.getElementById('container').innerHTML = '';
@@ -143,7 +161,7 @@ function listView () {
                 response.data.slice(firstEl, lastEl).forEach((photo) => {
                     container.appendChild(photoList(photo));
                     var p = document.getElementById('element-num');
-                    p.innerText =  `Mostrando ${lastEl} de 500`;;
+                    p.innerText =  `Mostrando ${lastEl} de 500`;
                 });
             })
             .catch((error) => {
