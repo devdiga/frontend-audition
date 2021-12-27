9a26f5336f413c58208aa6b07c3785a3888bd2b2
MichaelNascimentoFull <michaellopesn22@gmail.com>
2021-12-27 07:56:17 -0300

    Transferir parte do html e javascript da pagina para App.vue

--- src/app.js
+++ src/app.js
@@ -1,21 +1,6 @@
-import axios from 'axios';
+
 import { createApp } from 'vue'
 import App from './App.vue';
-import '/src/assets/page.css';
-
-import photoCard from './components/photoCard';
-
-axios.get('https://jsonplaceholder.typicode.com/photos')
-  .then((response) => {
-    const container = document.getElementById('container');
-    response.data.slice(0, 20).forEach((photo) => {
-      container.appendChild(photoCard(photo));
-    });
-  })
-  .catch((error) => {
-    alert(error);
-  });
-
 
 createApp(App).mount('#app')
 
