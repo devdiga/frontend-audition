fef45b8d6be9c639e4a4e6a362d83c96dcbdf08c
Lucas Zanotelli <lucaszanotelli@gmail.com>
2022-01-14 17:14:58 +0000

    create main app instance and root component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Image Library</title>
-</head>
-<body>
-  <div id="title">
+<template>
+<div id="title">
     Photo Library
     <div class="action-container">
       <input
@@ -18,6 +11,17 @@
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M4 10.5c-.83 0-1.5.67-1.5 1.5s.67 1.5 1.5 1.5 1.5-.67 1.5-1.5-.67-1.5-1.5-1.5zm0-6c-.83 0-1.5.67-1.5 1.5S3.17 7.5 4 7.5 5.5 6.83 5.5 6 4.83 4.5 4 4.5zm0 12c-.83 0-1.5.68-1.5 1.5s.68 1.5 1.5 1.5 1.5-.68 1.5-1.5-.67-1.5-1.5-1.5zM7 19h14v-2H7v2zm0-6h14v-2H7v2zm0-8v2h14V5H7z"/></svg>
     </div>
   </div>
-  <div id="container"></div>
-</body>
-</html>
+<div id="container"></div>
+</template>
+
+<script>
+  export default {
+  name: 'App',
+  props: { 
+  },
+}
+</script>
+
+<style>
+  @import './assets/page.css';
+</style>
--- src/app.js
+++ src/app.js
@@ -1,15 +1,4 @@
-import axios from 'axios';
-import '/src/assets/page.css';
+import { createApp } from 'vue'
+import App from './App.vue'
 
-import photoCard from './components/photoCard';
-
-axios.get('https://jsonplaceholder.typicode.com/photos')
-  .then((response) => {
-    const container = document.getElementById('container');
-    response.data.slice(0, 20).forEach((photo) => {
-      container.appendChild(photoCard(photo));
-    });
-  })
-  .catch((error) => {
-    alert(error);
-  });
+createApp(App).mount('body')
