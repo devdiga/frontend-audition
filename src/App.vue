c0609e003b9c1b348d87187c9db82788d2a3b1dc
Lucas Zanotelli <lucaszanotelli@gmail.com>
2022-01-14 19:29:53 +0000

    migrate photocard component to vue

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,77 @@
 <template>
 <div id="title">
-    Photo Library
-    <div class="action-container">
-      <input
-        id="search"
-        type="text"
-        placeholder="Busca"
-      />
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="active"><g fill-rule="evenodd"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 3v8h8V3H3zm6 6H5V5h4v4zm-6 4v8h8v-8H3zm6 6H5v-4h4v4zm4-16v8h8V3h-8zm6 6h-4V5h4v4zm-6 4v8h8v-8h-8zm6 6h-4v-4h4v4z"/></g></svg>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M4 10.5c-.83 0-1.5.67-1.5 1.5s.67 1.5 1.5 1.5 1.5-.67 1.5-1.5-.67-1.5-1.5-1.5zm0-6c-.83 0-1.5.67-1.5 1.5S3.17 7.5 4 7.5 5.5 6.83 5.5 6 4.83 4.5 4 4.5zm0 12c-.83 0-1.5.68-1.5 1.5s.68 1.5 1.5 1.5 1.5-.68 1.5-1.5-.67-1.5-1.5-1.5zM7 19h14v-2H7v2zm0-6h14v-2H7v2zm0-8v2h14V5H7z"/></svg>
-    </div>
+  Photo Library
+  <div class="action-container">
+    <input
+      id="search"
+      type="text"
+      placeholder="Busca"
+    />
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="active"><g fill-rule="evenodd"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 3v8h8V3H3zm6 6H5V5h4v4zm-6 4v8h8v-8H3zm6 6H5v-4h4v4zm4-16v8h8V3h-8zm6 6h-4V5h4v4zm-6 4v8h8v-8h-8zm6 6h-4v-4h4v4z"/></g></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M4 10.5c-.83 0-1.5.67-1.5 1.5s.67 1.5 1.5 1.5 1.5-.67 1.5-1.5-.67-1.5-1.5-1.5zm0-6c-.83 0-1.5.67-1.5 1.5S3.17 7.5 4 7.5 5.5 6.83 5.5 6 4.83 4.5 4 4.5zm0 12c-.83 0-1.5.68-1.5 1.5s.68 1.5 1.5 1.5 1.5-.68 1.5-1.5-.67-1.5-1.5-1.5zM7 19h14v-2H7v2zm0-6h14v-2H7v2zm0-8v2h14V5H7z"/></svg>
   </div>
-<div id="container"></div>
+</div>
+
+<PhotoCard :data='data' />
 </template>
 
 <script>
+import { ref, onMounted } from 'vue';
+import PhotoCard from './components/PhotoCard.vue'
   export default {
   name: 'App',
   props: { 
   },
+  components: { 
+    PhotoCard
+  },
+
+  setup () {
+    const data = ref(null);
+    const error = ref(null);
+
+    function fetchData() {        
+        return fetch('http://jsonplaceholder.typicode.com/photos', {
+            method: 'get',
+            headers: {
+            'content-type': 'application/json'
+            }
+        })
+            .then(res => {
+            // a non-200 response code
+            if (!res.ok) {
+                // create error instance with http status text
+                const error = new Error(res.statusText);
+                error.json = res.json();
+                throw error;
+            }
+            return res.json();
+            })
+            .then(json => {
+            // set the response data
+            data.value = json.slice(0,20);
+            })
+            .catch(err => {
+            error.value = err;
+            // in case a custom json error response was provided
+            if (err.json) {
+                return err.json.then(json => {
+                // set the json response message
+                error.value.message = json.message;
+                });
+            }
+            })
+        }
+
+    onMounted(() => {
+        fetchData()        
+    });
+
+    return {
+        data,
+        error
+      };  
+  }  
 }
 </script>
 
