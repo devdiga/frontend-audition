8ec881c79672a3163a91086c0606afc061bbc2d2
Rafael da Silva Pereira <rafaelsilvapereiraa@gmail.com>
2022-10-21 08:10:06 -0300

    Adicionado botão para carregar mais imagens

--- src/app.js
+++ src/app.js
@@ -1,4 +1,17 @@
 import loadPhotos from './utils/loadPhotos'
 import '/src/assets/page.css';
 
-loadPhotos();
+// Controlador do indice de carregamento das imagens
+let index = 0;
+
+// Evento para o botão de carregamento de mais imagens - Por enquanto, carregando de 20 em 20.
+let loadMoreButton = document.querySelector("svg.more");
+
+loadMoreButton.addEventListener("click", () => {
+    index += 20;
+    loadPhotos(index)
+} )
+
+
+loadPhotos(index);
+
