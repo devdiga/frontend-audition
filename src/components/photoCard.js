45225c11f45f0d4c30a3cf2765ac4d206a5351d0
Rafael da Silva Pereira <rafaelsilvapereiraa@gmail.com>
2022-10-21 09:45:47 -0300

    Melhoria estética - Primeira Letra Maiúscula nos Títulos

--- src/components/photoCard.js
+++ src/components/photoCard.js
@@ -4,7 +4,7 @@ export default function photoCard(item) {
   const img = document.createElement('img');
   img.src = item.url;
   const txt = document.createElement('div');
-  txt.innerHTML = item.title;
+  txt.innerHTML = item.title.charAt(0).toUpperCase() + item.title.slice(1);
   txt.className = 'title';
   const card = document.createElement('div');
   card.className = 'photo-card';
