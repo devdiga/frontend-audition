8cc00289d455dc88afffcbedb56220c17a595493
Alberto Araújo <albertoafaj@hotmail.com>
2022-10-24 20:19:15 -0300

    feat: Implemente a busca
    Ao digitar no campo de busca, as fotos devem ser filtradas de acordo com
    seu título.

--- src/App.vue
+++ src/App.vue
@@ -7,30 +7,7 @@
     </head>
     <body>
       <div id="app">
-        <div id="title">
-          Photo Library
-          <div class="action-container">
-            <input id="search" type="text" placeholder="Busca" />
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              class="active"
-            >
-              <g fill-rule="evenodd">
-                <path d="M0 0h24v24H0z" fill="none" />
-                <path
-                  d="M3 3v8h8V3H3zm6 6H5V5h4v4zm-6 4v8h8v-8H3zm6 6H5v-4h4v4zm4-16v8h8V3h-8zm6 6h-4V5h4v4zm-6 4v8h8v-8h-8zm6 6h-4v-4h4v4z"
-                />
-              </g>
-            </svg>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-              <path d="M0 0h24v24H0V0z" fill="none" />
-              <path
-                d="M4 10.5c-.83 0-1.5.67-1.5 1.5s.67 1.5 1.5 1.5 1.5-.67 1.5-1.5-.67-1.5-1.5-1.5zm0-6c-.83 0-1.5.67-1.5 1.5S3.17 7.5 4 7.5 5.5 6.83 5.5 6 4.83 4.5 4 4.5zm0 12c-.83 0-1.5.68-1.5 1.5s.68 1.5 1.5 1.5 1.5-.68 1.5-1.5-.67-1.5-1.5-1.5zM7 19h14v-2H7v2zm0-6h14v-2H7v2zm0-8v2h14V5H7z"
-              />
-            </svg>
-          </div>
-        </div>
+        <PageHeader v-model:searchFilter.sync="searchFilter" />
         <PhotoCard
           :data="photos"
           @wheel.passive="infiniteScroll"
@@ -46,20 +23,23 @@
 import axios from "axios";
 import DataLoading from "./components/DataLoading.vue";
 import PhotoCard from "./components/PhotoCard.vue";
+import PageHeader from "./components/PageHeader.vue";
 
 export default {
   name: "App",
   components: {
     PhotoCard,
     DataLoading,
+    PageHeader,
   },
   data() {
     return {
       photos: null,
       photosForFetch: 20,
+      photosTotal: 0,
+      searchFilter: "",
       pages: 1,
       waitScroll: false,
-      infinite: true,
       loading: true,
     };
   },
@@ -68,15 +48,27 @@ export default {
       axios
         .get("https://jsonplaceholder.typicode.com/photos")
         .then((r) => {
-          this.photos = r.data.filter((photo) => photo.id <= this.photosToView);
+          const photo = r.data.filter((item) => {
+            if (
+              item.title
+                .toLowerCase()
+                .startsWith(this.searchFilter.toLowerCase())
+            ) {
+              return item;
+            }
+          });
+          this.photos = photo;
+          this.photosTotal = photo.length;
+          this.photos = photo.slice(0, this.photosToView);
           this.loading = false;
         })
         .catch((error) => {
           alert(error);
         });
     },
+
     infiniteScroll() {
-      if (this.infinite) {
+      if (this.photosToView < this.photosTotal) {
         const scrolling =
           document.body.scrollHeight -
           window.scrollY -
@@ -107,6 +99,11 @@ export default {
   },
   unmounted() {
     window.removeEventListener("scroll", this.handleScroll);
+  },
+  watch: {
+    searchFilter() {
+      this.fetchData();
+    },
   },
 };
 </script>
