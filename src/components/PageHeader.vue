7137f13382f4fc78216a95564842d9d8cbd57ef1
Alberto Araújo <albertoafaj@hotmail.com>
2022-10-25 08:08:14 -0300

    deat: set view pattern
    The visualization type is defined in the icons next to the search
    field.

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -12,7 +12,8 @@
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
-        class="active"
+        :class="{ active: displayPattern }"
+        @click="changeDisplayPattern"
       >
         <g fill-rule="evenodd">
           <path d="M0 0h24v24H0z" fill="none" />
@@ -21,7 +22,12 @@
           />
         </g>
       </svg>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        :class="{ active: !displayPattern }"
+        @click="changeDisplayPattern"
+      >
         <path d="M0 0h24v24H0V0z" fill="none" />
         <path
           d="M4 10.5c-.83 0-1.5.67-1.5 1.5s.67 1.5 1.5 1.5 1.5-.67 1.5-1.5-.67-1.5-1.5-1.5zm0-6c-.83 0-1.5.67-1.5 1.5S3.17 7.5 4 7.5 5.5 6.83 5.5 6 4.83 4.5 4 4.5zm0 12c-.83 0-1.5.68-1.5 1.5s.68 1.5 1.5 1.5 1.5-.68 1.5-1.5-.67-1.5-1.5-1.5zM7 19h14v-2H7v2zm0-6h14v-2H7v2zm0-8v2h14V5H7z"
@@ -40,6 +46,7 @@ export default {
     return {
       photosFilter: this.searchFilter,
       searchText: "",
+      displayPattern: true,
     };
   },
   props: {
@@ -49,6 +56,9 @@ export default {
   methods: {
     search() {
       this.$emit("update:searchFilter", this.searchText);
+    },
+    changeDisplayPattern() {
+      this.displayPattern = !this.displayPattern;
     },
   },
 };
